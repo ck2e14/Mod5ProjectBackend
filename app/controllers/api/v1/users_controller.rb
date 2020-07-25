cf114873ef4d613180fd33fb9773ab46a80a06aa
ck2e14 <chriswkennedy@icloud.com>
2020-07-25 19:28:56 +0100

    whitespacing/indentation

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,52 +1,53 @@
 class Api::V1::UsersController < ApplicationController
 
-   def create
-     @user = User.create(user_params)
-     if @user.valid?
-       render json: { user: UserSerializer.new(@user), token: issue_token(user_id: @user.id) }, status: :created
-     else
-       render json: { error: 'failed to create user' }, status: :not_acceptable
-     end
-   end
- 
-   def login
-     user = User.find_by(username: login_params[:username])
-     if user&.authenticate(login_params[:password])
-       render json: { user: UserSerializer.new(user), token: issue_token(user_id: user.id) }
-     else
-       render json: { errors: ['Username or password incorrect'] }
-     end
-   end
- 
-   def show
-     @user = User.find(params[:id])
-     render json: @user
-   end
- 
-   def index
-     @users = User.all
-     render json: @users
-   end
- 
-   def validate
-     if logged_in
-       render json: { user: UserSerializer.new(@current_user), token: issue_token(user_id: @current_user.id) }
-     else
-       render json: { errors: ['Invalid token'] }, status: :not_accepted
-     end
-   end
- 
-   private
- 
-   def user_params
-     params.require(:user).permit(:username, :password, :password_confirmation)
-   end
- 
-   def login_params
-     params.require(:user).permit(:username, :password)
-   end
- end
- 
+  def create
+    @user = User.create(user_params)
+    if @user.valid?
+      render json: { user: UserSerializer.new(@user), token: issue_token(user_id: @user.id) }, status: :created
+    else
+      render json: { errors: ['failed to create user'] }, status: :not_acceptable
+    end
+  end
+# token issued via BCrypt Gem
+
+  def login
+    user = User.find_by(username: login_params[:username])
+    if user&.authenticate(login_params[:password])
+      render json: { user: UserSerializer.new(user), token: issue_token(user_id: user.id) }
+    else
+      render json: { errors: ['Username or password incorrect'] }
+    end
+  end
+
+  def show
+    @user = User.find(params[:id])
+    render json: @user
+  end
+
+  def index
+    @users = User.all
+    render json: @users
+  end
+
+  def validate
+    if logged_in
+      render json: { user: UserSerializer.new(@current_user), token: issue_token(user_id: @current_user.id) }
+    else
+      render json: { errors: ['Invalid token'] }, status: :not_accepted
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :password, :password_confirmation)
+  end
+
+  def login_params
+    params.require(:user).permit(:username, :password)
+  end
+end
+
 
 #  back up
 
